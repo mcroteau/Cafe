1a5e8fb05e9614a8dc1ce431ceff9671ad101abd
Mike Croteau <croteau.mike@gmail.com>
2020-09-19 20:01:26 -0800

    .

---
+++ src/main/java/xyz/cafe/model/Post.java
@@ -0,0 +1,59 @@
+package xyz.cafe.model;
+
+public class Post {
+
+    public long id;
+    public String title;
+    public String content;
+    public long userId;
+    public long dateCreated;
+    public String date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(long dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
