ec28cdfa31af87bff9b275ed7065c8f41159e922
Mike Croteau <croteau.mike@gmail.com>
2020-09-21 11:38:39 -0800

    .

--- src/main/java/xyz/cafe/web/post/CreateServlet.java
+++ src/main/java/xyz/cafe/web/post/CreateServlet.java
@@ -1,5 +1,9 @@
 package xyz.cafe.web.post;
 
+import io.github.mcroteau.Parakeet;
+import xyz.cafe.common.Constants;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +14,14 @@ public class CreateServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/jsp/post/create.jsp").forward(req, resp);
+        ServletContext context = req.getServletContext();
+        Parakeet parakeet = (Parakeet) context.getAttribute(Constants.PARAKEET_LOOKUP);
+
+        if(parakeet.isAuthenticated()){
+            req.getRequestDispatcher("/jsp/post/create.jsp").forward(req, resp);
+        }else{
+            resp.sendRedirect(context.getContextPath() + "/signin");
+        }
     }
+
 }
